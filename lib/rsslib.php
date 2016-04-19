f29b97e344021ffa6c6a280cee9b3ebc8e8f9052
Chris Miller <chris@intellagentbenefits.com>
2016-04-19 11:23:52 -0500

    first pass of cleaned proxy code.

--- lib/rsslib.php
+++ lib/rsslib.php
@@ -1,23 +1,4 @@
 <?php
-/*
-	RSS Extractor and Displayer
-	(c) 2007-2010  Scriptol.com - Licence Mozilla 1.1.
-	rsslib.php
-	
-	Requirements:
-	- PHP 5.
-	- A RSS feed.
-	
-	Using the library:
-	Insert this code into the page that displays the RSS feed:
-	
-	<?php
-	require_once("rsslib.php");
-	echo RSS_Display("http://www.xul.fr/rss.xml", 15);
-	? >
-	
-*/
-
 $RSS_Content = array();
 
 function RSS_Tags($item, $type)
@@ -48,46 +29,6 @@ function RSS_Tags($item, $type)
 		return $y;
 }
 
-
-function RSS_Channel($channel)
-{
-	global $RSS_Content;
-
-	$items = $channel->getElementsByTagName("item");
-	
-	// Processing channel
-	
-	$y = RSS_Tags($channel, 0);		// get description of channel, type 0
-	array_push($RSS_Content, $y);
-	
-	// Processing articles
-	
-	foreach($items as $item)
-	{
-		$y = RSS_Tags($item, 1);	// get description of article, type 1
-		array_push($RSS_Content, $y);
-	}
-}
-
-function RSS_Retrieve($url)
-{
-	global $RSS_Content;
-
-	$doc  = new DOMDocument();
-	$doc->load($url);
-
-	$channels = $doc->getElementsByTagName("channel");
-	
-	$RSS_Content = array();
-	
-	foreach($channels as $channel)
-	{
-		 RSS_Channel($channel);
-	}
-	
-}
-
-
 function RSS_RetrieveLinks($url)
 {
 	global $RSS_Content;
@@ -113,122 +54,16 @@ function RSS_RetrieveLinks($url)
 }
 
 
-function RSS_Links($url, $size = 15)
+function RSS_Jsonify($url, $size = 10)
 {
 	global $RSS_Content;
-
-	$page = "<ul class=\"rss_links\" id=\"animateList\">";
 
 	RSS_RetrieveLinks($url);
 	if($size > 0)
 		$recents = array_slice($RSS_Content, 0, $size + 1);
 
-	$bgc = "0";
-
-	foreach($recents as $article)
-	{
-		$type = $article["type"];
-		if($type == 0) continue;
-		$title = $article["title"];
-		$link = $article["link"];
-		$page .= "<li><a href=\"$link\" target='_new'>$title</a></li>\n";			
-	}
-
-	$page .="</ul>\n";
-
-	return $page;
+	return $recents;
 	
 }
-
-function RSS_MobileLinks($url, $size = 15)
-{
-	global $RSS_Content;
-
-	$page = "<ul class=\"rss_links\">";
-
-	RSS_RetrieveLinks($url);
-	if($size > 0)
-		$recents = array_slice($RSS_Content, 0, $size + 1);
-
-	foreach($recents as $article)
-	{
-
-		$type = $article["type"];
-		if($type == 0) continue;
-		$title = $article["title"];
-		$link = $article["link"];
-		$page .= "<li><a href=\"$link\" target='_new'>$title</a></li>\n";			
-	}
-
-	$page .="</ul>\n";
-
-	return $page;
-	
-}
-
-
-
-function RSS_Display($url, $size = 15, $site = 0, $withdate = 0)
-{
-	global $RSS_Content;
-
-	$opened = false;
-	$page = "";
-	$site = (intval($site) == 0) ? 1 : 0;
-
-	RSS_Retrieve($url);
-	if($size > 0)
-		$recents = array_slice($RSS_Content, $site, $size + 1 - $site);
-
-	foreach($recents as $article)
-	{
-		$type = $article["type"];
-		if($type == 0)
-		{
-			if($opened == true)
-			{
-				$page .="</ul>\n";
-				$opened = false;
-			}
-			$page .="<b>";
-		}
-		else
-		{
-			if($opened == false) 
-			{
-				$page .= "<ul>\n";
-				$opened = true;
-			}
-		}
-		$title = $article["title"];
-		$link = $article["link"];
-		$page .= "<li><a href=\"$link\">$title</a>";
-		if($withdate)
-		{
-      $date = $article["date"];
-      $page .=' <span class="rssdate">'.$date.'</span>';
-    }
-		$description = $article["description"];
-		if($description != false)
-		{
-			$page .= "<br><span class='rssdesc'>$description</span>";
-		}
-		$page .= "</li>\n";			
-		
-		if($type==0)
-		{
-			$page .="</b><br />";
-		}
-
-	}
-
-	if($opened == true)
-	{	
-		$page .="</ul>\n";
-	}
-	return $page."\n";
-	
-}
-
 
 ?>
